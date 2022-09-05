70f47414b980d2ebd4a9b2a7652f3e24d2f93fcf
Jun Guo <gjuoun@gmail.com>
2022-09-05 03:22:53 -0300

    added nextjs

---
+++ apps/nextjs-playground/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      Hello, Jun
+      <div>Hey This is the content</div>
+    </div>
+  )
+}
+
+export default Home
